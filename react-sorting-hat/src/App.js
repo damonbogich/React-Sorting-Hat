44a9057258f37de9cbd76fdffad00167f8a1f2f1
Damon Bogich <damonbogich@gmail.com>
2021-01-25 11:28:37 -0700

    form values updating state

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Greeting from './Components/Greeting';
+import Form from './Components/Form';
+
 import {Route} from 'react-router-dom';
 
 import {hogwartsHouses} from './data';
 import './App.css';
 
 function App() {
+  const [formValues, setFormValues] = useState({
+    1: null,
+    2: null,
+    3: null
+  });
+
+  //handleSelect to increment the count of the house value after an option is selected
+  const handleSelect = (e) => {
+    let index = e.target.name
+    console.log(e.target.value)
+    console.log(e.target.name)
+    let newArray = formValues;
+    newArray[index] = e.target.value;
+    console.log(newArray)
+    setFormValues(newArray)
+    console.log(formValues, 'formvalues');
+  };
+  //handleSubmit will push the user to whichever house they're selected for
+  
   return (
     <div className="App">
       <Route exact path="/">
         <Greeting />
+      </Route>
+      <Route path="/form">
+        <Form handleSelect={handleSelect} formValues={formValues}/>
       </Route>
     </div>
   );
--- react-sorting-hat/src/Components/Form.js
+++ react-sorting-hat/src/Components/Form.js
@@ -1,1 +1,36 @@
 //questions that will determine which house the user will be put into.
+import React from 'react';
+export default function Form({handleSelect, formValues}) {
+    console.log(formValues)
+    return (
+        <form>
+            <label> Which of the following do you value most?
+                <select name={1} value={formValues[0]} onChange={(e) => handleSelect(e)}>
+                    <option value="Griffindor">Bravery</option>
+                    <option value="Hufflepuff">Dedication</option>
+                    <option value="Ravenclaw">Intelligence</option>
+                    <option value="Slytherin">Ambition</option>
+                </select>
+            </label>
+            <label> Which element do you identify with most?
+                <select name={2} value={formValues[1]} onChange={(e) => handleSelect(e)}>
+                    <option value="Griffindor">Fire</option>
+                    <option value="Hufflepuff">Earth</option>
+                    <option value="Ravenclaw">Air</option>
+                    <option value="Slytherin">Water</option>
+                </select>
+            </label>
+            <label> Choose your favorite:
+                <select name={3} value={formValues[2]} onChange={(e) => handleSelect(e)}>
+                    <option value="Griffindor">Harry Potter</option>
+                    <option value="Hufflepuff">Newt Scamander</option>
+                    <option value="Ravenclaw">Luna Lovegood</option>
+                    <option value="Slytherin">Draco Malfoy</option>
+                </select>
+            </label>
+            <button>Submit</button> 
+            {/* //when the form is submitted, we want the value of each option to be counted 
+            so set the values to state........????*/}
+        </form>
+    )
+}
