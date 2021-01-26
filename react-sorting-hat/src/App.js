e70884b4047ed22fc64941e961d3d443172f3802
Damon Bogich <damonbogich@gmail.com>
2021-01-25 17:06:19 -0700

    can find users house

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -14,12 +14,11 @@ function App() {
     2: "",
     3: ""
   });
+  const [userHouse, setUserHouse] = useState("")
 
   //handleSelect to increment the count of the house value after an option is selected
   const handleSelect = (e) => {
     let index = e.target.name
-    console.log(e.target.value)
-    console.log(e.target.name)
     let newArray = formValues;
     newArray[index] = e.target.value;
     console.log(newArray)
@@ -27,14 +26,39 @@ function App() {
     console.log(formValues, 'formvalues');
   };
   //handleSubmit will push the user to whichever house they're selected for
-  
+  const findHouse = (e) => {
+    e.preventDefault();
+    let counter = {Gryffindor: 0, Hufflepuff: 0, Ravenclaw: 0, Slytherin: 0,}
+ 
+    let listOfValues = Object.values(formValues);
+
+    listOfValues.forEach((item) => {
+      if(item[0] === 'G') {
+        counter.Gryffindor += 1
+      }
+      else if (item[0] === 'H') {
+        counter.Hufflepuff += 1
+      }
+      else if (item[0] === 'R') {
+        counter.Ravenclaw += 1
+      }
+      else if (item[0] === 'S') {
+        counter.Slytherin += 1
+      }
+      return counter
+    })
+    console.log(counter, 'countercounter');
+    let highestVal = Object.keys(counter).reduce(function(a, b){ return counter[a] > counter[b] ? a : b });
+    console.log(highestVal, typeof(highestVal), 'highest value')
+    setUserHouse(highestVal);
+  }
   return (
     <div className="App">
       <Route exact path="/">
         <Greeting />
       </Route>
       <Route path="/form">
-        <Form handleSelect={handleSelect} formValues={formValues}/>
+        <Form findHouse={findHouse} handleSelect={handleSelect} formValues={formValues}/>
       </Route>
     </div>
   );
--- react-sorting-hat/src/Components/Form.js
+++ react-sorting-hat/src/Components/Form.js
@@ -1,33 +1,34 @@
 //questions that will determine which house the user will be put into.
 import React from 'react';
-export default function Form({handleSelect, formValues}) {
+export default function Form({findHouse, handleSelect, formValues}) {
     console.log(formValues)
     return (
         <form>
             <label> Which of the following do you value most?
-                <select name={1} value={formValues[0]} onChange={(e) => handleSelect(e)}>
-                    <option value="Griffindor">Bravery</option>
+                <select name={1} value={formValues[1]} onChange={(e) => handleSelect(e)}>
+                    <option value="Gryffindor">Bravery</option>
                     <option value="Hufflepuff">Dedication</option>
                     <option value="Ravenclaw">Intelligence</option>
                     <option value="Slytherin">Ambition</option>
                 </select>
             </label>
             <label> Which element do you identify with most?
-                <select name={2} value={formValues[1]} onChange={(e) => handleSelect(e)}>
-                    <option value="Griffindor">Fire</option>
+                <select name={2} value={formValues[2]} onChange={(e) => handleSelect(e)}>
+                    <option value="Gryffindor">Fire</option>
                     <option value="Hufflepuff">Earth</option>
                     <option value="Ravenclaw">Air</option>
                     <option value="Slytherin">Water</option>
                 </select>
             </label>
             <label> Choose your favorite:
-                <select name={3} value={formValues[2]} onChange={(e) => handleSelect(e)}>
-                    <option value="Griffindor">Harry Potter</option>
+                <select name={3} value={formValues[3]} onChange={(e) => handleSelect(e)}>
+                    <option value="Gryffindor">Harry Potter</option>
                     <option value="Hufflepuff">Newt Scamander</option>
                     <option value="Ravenclaw">Luna Lovegood</option>
                     <option value="Slytherin">Draco Malfoy</option>
                 </select>
             </label>
+            <button onClick={(e) => findHouse(e)}>Find Your house</button>
             <button>Submit</button> 
             {/* //when the form is submitted, we want the value of each option to be counted 
             so set the values to state........????*/}
